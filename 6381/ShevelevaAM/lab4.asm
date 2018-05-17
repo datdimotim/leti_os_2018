9b35523cafd1419d13aada123d826f3c89c9354f
ShevelevaAnna <myfirst@myfirst.com>
2018-05-17 15:45:10 +0300

    Лабораторная полностью переделана

--- 6381/ShevelevaAM/lab4.asm
+++ 6381/ShevelevaAM/lab4.asm
@@ -1,236 +1,255 @@
+ISTACK SEGMENT STACK
+	dw 100h dup (?)
+ISTACK ENDS
 
 CODE SEGMENT
-	ASSUME CS:CODE, DS:CODE , SS:STACKSG
+ ASSUME CS:CODE, DS:DATA, ES:DATA, SS:STACK
+START: JMP BEGIN
 
-ROUT PROC FAR
-
-jmp start
-	String		DB		'Error'
-	Symbol		DB		0
-	Save_CS		DW		0 
-	Save_IP		DW		0 
-	Save_PSP	DW		0	
-
-start:	
-	push ss
-	push sp
-
+setterCurs PROC
 	push ax
 	push bx
 	push dx
 	push cx
+	mov ah,02h
+	mov bh,0
+	;mov dh,0 ; DH, DL = строка, колонка
+	;mov dl,60
 
-	mov ah,03h
-	mov bh,0
-	int 10h 
-	push dx   ;текущие строка, колонка курсора
-	
-
-	mov ah,02h   ;передвижение курсора
-	mov bh,0
-	mov dh, 20
-	mov dl,40
-	int 10h 
-	
-	mov al, Symbol	;вывод символа	
-	add al, 1
-	mov Symbol, al
-	mov ah,09h 
-	mov bh,0 
-	mov bl, 10
-	mov cx,1 
-	int 10h 
-	
-	mov ah,02h  ;возвращение курсора
-	mov bh,0
-	pop DX
-	int 10h 
-	
-	pop CX
+	int 10h
+	pop cx
 	pop dx
 	pop bx
 	pop ax
+	ret
+setterCurs ENDP
+
+getterCurs PROC
+	push ax
+	push bx
+	push cx
+	mov ah,03h
+	mov bh,0
+	int 10h
+	pop cx
+	pop bx
+	pop ax
+	ret
+getterCurs ENDP
+
+output PROC
+	push ax
+	push bx
+	push cx
+	mov ah,09h  
+	mov bh,0  
+	mov bl,07h
+	mov cx,1  
+	int 10h    
+	pop cx
+	pop bx
+	pop ax
+	ret
+output ENDP
+
+ROUT PROC FAR
+	jmp INT_CODE
+	KEEP_IP DW 0
+	KEEP_CS DW 0
+	KEEP_PSP DW 0
+	COUNT DB 0
+	KEEP_SS DW 0
+	KEEP_SP DW 0
+	KEEP_AX DW 0
+	SIGNATURE db 'CARTOON_IS_LOVE$'
+	INT_CODE:
+
+	mov KEEP_AX, ax
+	mov CS:KEEP_SS, ss
+	mov CS:KEEP_SP, sp
+	mov ax, ISTACK
+	mov ss, ax
+	mov sp, 100h
+	push dx
+	push ds
+	push es
+
+	call getterCurs
+	push dx
+	mov dh,0 ; DH, DL = строка, колонка
+	mov dl,79
+	call setterCurs
+
+	cmp COUNT,0AH
+	jl rout_skip
+	mov count,0h
+	rout_skip:
+	mov al,COUNT
+	or al,30h
+	call output
+
+	pop dx
+	call setterCurs
+	inc COUNT
+
+	pop es
+	pop ds
+	pop dx
+	mov ax, KEEP_AX
+	mov al,20h
+	out 20h,al
+	mov sp, KEEP_SP
+	mov ss, KEEP_SS
 	
-	pop sp
-	pop ss
-	
-	MOV AL, 20H
-	OUT 20H,AL
-		
-	IRET
-	
+	iret
 ROUT ENDP
-DW 20 DUP(?)
 LAST_BYTE:
 
-CHECK_TAIL PROC NEAR   ;проверка наличия строки /un в хвосте программы 
+CHECK_INT PROC
+    mov ah,35h
+    mov al,1ch
+    int 21h 
+	
+	mov si, offset SIGNATURE
+	sub si, offset ROUT
+	mov di, offset STR_SIGNATURE
+	mov cx, 19 ; len of SIGNATURE
 
-	push dx
-	push cx
-	push si
-	
-	mov ah, 02h
-	
-	mov cl, es:[80h]		;число символов в хвосте	
-	cmp cl, 4
-	jne another_tail
-	
-	mov si, 82h
-	
-	mov dl, es:[si]		;1ый символ
-	cmp dl, '/'
-	jne another_tail
-	
-	inc si
-	mov dl, es:[si]		;2ой символ
-	cmp dl, 'u'
-	jne another_tail
-	
-	inc si
-	mov dl, es:[si]		;3ий символ
-	cmp dl, 'n'
-	jne another_tail
-	
-	pop si
-	pop	cx
-	pop dx
-	
-	mov al, 1
-	ret
+	label_rabel:
+		mov ah, [di]
+		cmp ah, es:[bx+si]
+		jne NOT_LOADED
+		inc di
+		inc si
+	loop label_rabel
 
-another_tail:
-	pop si
-	pop	cx
-	pop dx
+	jmp LOADED 
 	
-	mov al, 0
-	ret
-CHECK_TAIL ENDP
+NOT_LOADED:
 
-ERR	DB "! Handler is already set!", '$'
+    lea dx, STR_LOADED
+    call PRINT
 
-MAIN PROC near
+    call SET_INT
 
-	mov ax, CODE
-	mov ds, ax
-	
-	mov ax, 2
-	call CHECK_TAIL
-	
-	cmp al, 1
-	je un
-	
-	mov Save_PSP, es   ;сохранение PSP 
-	mov ah, 35H
-	mov al, 1CH 
-	int 21H
-	mov Save_IP, bx 
-	mov Save_CS, es 
-	
-	mov ah, 2	;проверка, установлен ли обработчик
-	mov si, 3
-	mov dl, es:[si]
-	int 21h
-	cmp dl, 'E'
-	jne continue
-	
-	inc si
-	mov dl, es:[si]
-	int 21h
-	cmp dl, 'r'
-	jne continue
-	
-	inc si
-	mov dl, es:[si]
-	int 21h
-	cmp dl, 'r'
-	jne continue
-	
-	inc si
-	mov dl, es:[si]
-	int 21h
-	cmp dl, 'o'
-	jne continue
-	
-	inc si
-	mov dl, es:[si]
-	int 21h
-	cmp dl, 'r'
-	jne continue
-	
+    mov dx,offset LAST_BYTE 
+    mov cl,4
+    shr dx,cl
+    inc dx
+    add dx,CODE 
+    sub dx,KEEP_PSP 
 
-	mov ax, CODE
-	mov ds, ax
-	lea dx, ERR
-	mov ah, 09h
-	int 21h
-	
-	jmp exit
-	
-continue:
-	push ds					;устанавливаем написанное прерывание в поле векторов прерываний		
-	mov dx, OFFSET ROUT					 
-	mov ax, SEG ROUT 
-	mov ds, ax 
-	mov ah, 25H 
-	mov al, 1CH 
-	int 21H 
-	pop ds
+    xor al,al
+    mov ah,31h
+    int 21h
+		
+LOADED:
+    push es
+    push bx
+    mov bx,KEEP_PSP
+    mov es,bx
 
-	mov dx, OFFSET LAST_BYTE + 100h  ;оставляем процедуру прерывания резидентной в памяти
-	mov cl,4
-	shr dx,cl
-	inc dx
-	mov ah, 31h
-	int 21h
-	
-un:
-	mov ah, 35h
-	mov al, 1Ch 
-	int 21h
+    cmp byte ptr es:[82h],'/'
+    jne CI_DONT_DELETE
+    cmp byte ptr es:[83h],'u'
+    jne CI_DONT_DELETE
+    cmp byte ptr es:[84h],'n'
+    je CI_DELETE
 
-	cli  ;выгружаем обработчик
+CI_DONT_DELETE:
+    pop bx
+    pop es
+    mov dx,offset STR_ALR_LOADED
+    call PRINT
+
+    ret
+
+CI_DELETE:
+    pop bx
+    pop es
+    call DEL_INT
+
+    mov dx,offset STR_UNLOADED
+    call PRINT
+
+    ret
+CHECK_INT ENDP
+
+DEL_INT PROC
 	push ds
-	mov si, 11
-	mov dx, es:[si] 
-	mov si, 9
-	mov ax, es:[si] 
-	mov ds, ax
-	mov ah, 25h
-	mov al, 1Ch
-	int 21h
-	pop ds
-	sti
-	
-	mov si, 13
-	mov ax, es:[si]
-	push ax
-	mov es, ax
-	
-	mov si, 2Ch
-	mov ax, es:[si]
-	mov es, ax
-	
-	mov ah, 49h		
-	int 21h	
-	
-	pop es			
-	mov ah, 49h	
-	int 21h	
-	
-exit:	
-	; выход в DOS
-	xor al,al
-	mov ah,4Ch
+	CLI
+	mov dx,ES:[BX+3] ; IP
+	mov ax,ES:[BX+5] ; CS
+	mov ds,ax
+	mov ax,251ch
+	int 21h 
+
+	push es
+	mov ax,ES:[BX+7] ; PSP
+	mov es,ax 
+	mov es,es:[2Ch]
+	mov ah,49h         
 	int 21h
 
-MAIN ENDP
+	pop es
+	mov es,ES:[BX+7] ; PSP
+	mov ah, 49h
+	int 21h	
+	STI
+	pop ds
+
+	ret
+DEL_INT ENDP
+
+SET_INT PROC
+	push ds
+	mov ah,35h
+	mov al,1ch
+	int 21h
+	mov KEEP_IP,bx
+	mov KEEP_CS,es
+
+	mov dx,offset ROUT
+	mov ax,seg ROUT
+	mov ds,ax
+	mov ah,25h
+	mov al,1ch
+	int 21h
+	pop ds
+	
+    ret
+SET_INT ENDP 
+
+PRINT PROC near
+	push ax
+	mov al,00h
+	mov AH,09h
+	int 21h
+	pop ax
+	ret
+PRINT ENDP
+
+BEGIN:
+	mov ax,data
+	mov ds,ax
+	mov KEEP_PSP,es
+	
+	call CHECK_INT
+	
+	mov AX,4C00h
+	int 21H
+	
 CODE ENDS
 
-STACKSG	SEGMENT STACK 
-	DW 64 DUP(?)
-STACKSG	ENDS
-	END MAIN
-	
+STACK SEGMENT STACK
+	dw 100h dup (?)
+STACK ENDS
 
-	
+DATA SEGMENT
+    STR_SIGNATURE DB 'CARTOON_IS_LOVE$'
+	STR_ALR_LOADED DB 'User interruption is already loaded',0DH,0AH,'$'
+	STR_UNLOADED DB 'User interruption is successfully unloaded',0DH,0AH,'$'
+	STR_LOADED DB 'User interruption is loaded',0DH,0AH,'$'
+DATA ENDS
+
+END START
